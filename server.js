db5230462f6b13c2f87aba48ae85bc8956558167
eduardo rivas <r.eduardo92@gmail.com>
2019-11-16 12:39:43 -0600

    install cors

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const log = console.log;
 
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
 
+// Init Cors
+app.use(cors());
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -23,14 +27,11 @@ app.use('/api/food', require('./routes/food'));
 
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set satic folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => log(`server started on port ${PORT}`));
