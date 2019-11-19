36e8f18cdec7abb5b52b35a4d303bab08636c381
eduardo rivas <r.eduardo92@gmail.com>
2019-11-18 19:01:00 -0600

    changes to db env

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // const config = require('config');
 
 // const db = config.get('mongoURI');
-const db = process.env.mongoURI;
+const db = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
